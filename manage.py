afba58b9f01c211cde1d7838d60873c883109b6e
csmatthew <christopher.s.matthew@gmail.com>
2025-02-03 18:49:09 +0000

    new secret key, config settings.py

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monastery.settings')
     try:
+        import env  # Add this line to import env.py
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
